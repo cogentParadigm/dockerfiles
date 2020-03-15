3f4ddb6d7db1460ce5127c41406d4a3c91e783e4
Ali Gangji <ali@neonrain.com>
2020-03-15 13:14:01 -0600

    php image with user creation script and patch command

---
+++ php/bin/precreate-user.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+id -g ${PHP_GROUP} || groupadd -g ${PHP_GID} ${PHP_GROUP}
+id -u ${PHP_USER} || useradd -m -u ${PHP_UID} -g ${PHP_GID} ${PHP_USER}
+sed -i "s/^user = .*/user = ${PHP_USER}/" /etc/php-fpm.d/www.conf
+sed -i "s/^group = .*/group = ${PHP_GROUP}/" /etc/php-fpm.d/www.conf
+$@
